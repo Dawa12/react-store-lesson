31ca6683d61b308d4103dfca447000df53cce97f
Tisha Greenidge <tisha.m.greenidge@gmail.com>
2018-03-15 11:21:04 -0400

    add Admin page and ProductList Component

---
+++ src/components/AdminView.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class AdminView extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h1>Admin View</h1>
+        <h2>Products</h2>
+        <h2>Create a new Product</h2>
+      </div>
+    );
+  }
+}
+
+export default AdminView;
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+import AdminView from './AdminView';
 
 class HomePage extends Component {
   constructor() {
     super();
     this.state = {
       itemCurrentlyOnSale: 'Glue',
-      editSaleItem: true
+      editSaleItem: true,
+      productList: [
+        {
+          productName: 'Hammer',
+          description: "It's a hammer!",
+          price: 12.3
+        },
+        {
+          productName: 'Nail',
+          description: "It's a nail!",
+          price: 0.12
+        }
+      ]
     };
   }
 
@@ -14,6 +27,10 @@ class HomePage extends Component {
     this.setState({ editSaleItem });
   };
 
+  handleItemCurrentlyOnSaleChange = e => {
+    const itemCurrentlyOnSale = e.target.value;
+    this.setState({ itemCurrentlyOnSale: itemCurrentlyOnSale });
+  };
   render() {
     let buttonText = this.state.editSaleItem ? 'Hide' : 'Edit Sale Item';
 
@@ -28,9 +45,15 @@ class HomePage extends Component {
 
           {this.state.editSaleItem ? (
             <div>
-              <input type="text" />
+              <input
+                type="text"
+                value={this.state.itemCurrentlyOnSale}
+                onChange={this.handleItemCurrentlyOnSaleChange}
+              />
             </div>
           ) : null}
+
+          <AdminView productList={this.state.productList} />
         </div>
       </div>
     );
